0d107704bb57e9bf48694d880185116b606640c4
Polo <poloaguirre3421@gmail.com>
2022-03-28 21:00:37 -0600

    Second commits

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,176 +1,202 @@
-import User from '../models/user.model'
-import extend from 'lodash/extend'
-import errorHandler from './../helpers/dbErrorHandler'
-import request from 'request'
-import config from './../../config/config'
-import stripe from 'stripe'
+import User from "../models/user.model";
+import extend from "lodash/extend";
+import errorHandler from "./../helpers/dbErrorHandler";
+import request from "request";
+import config from "./../../config/config";
+import stripe from "stripe";
 
-const myStripe = stripe(config.stripe_test_secret_key)
+const myStripe = stripe(config.stripe_test_secret_key);
 
 const create = async (req, res) => {
-  const user = new User(req.body)
+  const user = new User(req.body);
   try {
-    await user.save()
+    await user.save();
     return res.status(200).json({
-      message: "Successfully signed up!"
-    })
+      message: "Successfully signed up!",
+    });
   } catch (err) {
     return res.status(400).json({
-      error: errorHandler.getErrorMessage(err)
-    })
+      error: errorHandler.getErrorMessage(err),
+    });
   }
-}
+};
 
 /**
  * Load user and append to req.
  */
 const userByID = async (req, res, next, id) => {
   try {
-    let user = await User.findById(id)
+    let user = await User.findById(id);
     if (!user)
-      return res.status('400').json({
-        error: "User not found"
-      })
-    req.profile = user
-    next()
+      return res.status("400").json({
+        error: "User not found",
+      });
+    req.profile = user;
+    next();
   } catch (err) {
-    return res.status('400').json({
-      error: "Could not retrieve user"
-    })
+    return res.status("400").json({
+      error: "Could not retrieve user",
+    });
   }
-}
+};
 
 const read = (req, res) => {
-  req.profile.hashed_password = undefined
-  req.profile.salt = undefined
-  return res.json(req.profile)
-}
+  req.profile.hashed_password = undefined;
+  req.profile.salt = undefined;
+  return res.json(req.profile);
+};
 
 const list = async (req, res) => {
   try {
-    let users = await User.find().select('name email updated created')
-    res.json(users)
+    let users = await User.find().select("name email updated created");
+    res.json(users);
   } catch (err) {
     return res.status(400).json({
-      error: errorHandler.getErrorMessage(err)
-    })
+      error: errorHandler.getErrorMessage(err),
+    });
   }
-}
+};
 
 const update = async (req, res) => {
   try {
-    let user = req.profile
-    user = extend(user, req.body)
-    user.updated = Date.now()
-    await user.save()
-    user.hashed_password = undefined
-    user.salt = undefined
-    res.json(user)
+    let user = req.profile;
+    user = extend(user, req.body);
+    user.updated = Date.now();
+    await user.save();
+    user.hashed_password = undefined;
+    user.salt = undefined;
+    res.json(user);
   } catch (err) {
     return res.status(400).json({
-      error: errorHandler.getErrorMessage(err)
-    })
+      error: errorHandler.getErrorMessage(err),
+    });
   }
-}
+};
 
 const remove = async (req, res) => {
   try {
-    let user = req.profile
-    let deletedUser = await user.remove()
-    deletedUser.hashed_password = undefined
-    deletedUser.salt = undefined
-    res.json(deletedUser)
+    let user = req.profile;
+    let deletedUser = await user.remove();
+    deletedUser.hashed_password = undefined;
+    deletedUser.salt = undefined;
+    res.json(deletedUser);
   } catch (err) {
     return res.status(400).json({
-      error: errorHandler.getErrorMessage(err)
-    })
+      error: errorHandler.getErrorMessage(err),
+    });
   }
-}
+};
 
 const isSeller = (req, res, next) => {
-  const isSeller = req.profile && req.profile.seller
+  const isSeller = req.profile && req.profile.seller;
   if (!isSeller) {
-    return res.status('403').json({
-      error: "User is not a seller"
-    })
+    return res.status("403").json({
+      error: "User is not a seller",
+    });
   }
-  next()
-}
+  next();
+};
 
 const stripe_auth = (req, res, next) => {
-  request({
-    url: "https://connect.stripe.com/oauth/token",
-    method: "POST",
-    json: true,
-    body: {client_secret:config.stripe_test_secret_key,code:req.body.stripe, grant_type:'authorization_code'}
-  }, (error, response, body) => {
-    //update user
-    if(body.error){
-      return res.status('400').json({
-        error: body.error_description
-      })
+  request(
+    {
+      url: "https://connect.stripe.com/oauth/token",
+      method: "POST",
+      json: true,
+      body: {
+        client_secret: config.stripe_test_secret_key,
+        code: req.body.stripe,
+        grant_type: "authorization_code",
+      },
+    },
+    (error, response, body) => {
+      //update user
+      if (body.error) {
+        return res.status("400").json({
+          error: body.error_description,
+        });
+      }
+      req.body.stripe_seller = body;
+      next();
     }
-    req.body.stripe_seller = body
-    next()
-  })
-}
+  );
+};
 
 const stripeCustomer = (req, res, next) => {
-  if(req.profile.stripe_customer){
-      //update stripe customer
-      myStripe.customers.update(req.profile.stripe_customer, {
-          source: req.body.token
-      }, (err, customer) => {
-        if(err){
+  if (req.profile.stripe_customer) {
+    //update stripe customer
+    myStripe.customers.update(
+      req.profile.stripe_customer,
+      {
+        source: req.body.token,
+      },
+      (err, customer) => {
+        if (err) {
           return res.status(400).send({
-            error: "Could not update charge details"
-          })
+            error: "Could not update charge details",
+          });
         }
-        req.body.order.payment_id = customer.id
-        next()
+        req.body.order.payment_id = customer.id;
+        next();
+      }
+    );
+  } else {
+    myStripe.customers
+      .create({
+        email: req.profile.email,
+        source: req.body.token,
       })
-  }else{
-      myStripe.customers.create({
-            email: req.profile.email,
-            source: req.body.token
-      }).then((customer) => {
-          User.update({'_id':req.profile._id},
-            {'$set': { 'stripe_customer': customer.id }},
-            (err, order) => {
-              if (err) {
-                return res.status(400).send({
-                  error: errorHandler.getErrorMessage(err)
-                })
-              }
-              req.body.order.payment_id = customer.id
-              next()
-            })
-      })
+      .then((customer) => {
+        User.update(
+          { _id: req.profile._id },
+          { $set: { stripe_customer: customer.id } },
+          (err, order) => {
+            if (err) {
+              return res.status(400).send({
+                error: errorHandler.getErrorMessage(err),
+              });
+            }
+            req.body.order.payment_id = customer.id;
+            next();
+          }
+        );
+      });
   }
-}
+};
 
 const createCharge = (req, res, next) => {
-  if(!req.profile.stripe_seller){
-    return res.status('400').json({
-      error: "Please connect your Stripe account"
-    })
+  if (!req.profile.stripe_seller) {
+    return res.status("400").json({
+      error: "Please connect your Stripe account",
+    });
   }
-  myStripe.tokens.create({
-    customer: req.order.payment_id,
-  }, {
-    stripeAccount: req.profile.stripe_seller.stripe_user_id,
-  }).then((token) => {
-      myStripe.charges.create({
-        amount: req.body.amount * 100, //amount in cents
-        currency: "usd",
-        source: token.id,
-      }, {
+  myStripe.tokens
+    .create(
+      {
+        customer: req.order.payment_id,
+      },
+      {
         stripeAccount: req.profile.stripe_seller.stripe_user_id,
-      }).then((charge) => {
-        next()
-      })
-  })
-}
+      }
+    )
+    .then((token) => {
+      myStripe.charges
+        .create(
+          {
+            amount: req.body.amount * 100, //amount in cents
+            currency: "usd",
+            source: token.id,
+          },
+          {
+            stripeAccount: req.profile.stripe_seller.stripe_user_id,
+          }
+        )
+        .then((charge) => {
+          console.log(charge);
+          next();
+        });
+    });
+};
 
 export default {
   create,
@@ -182,5 +208,5 @@ export default {
   isSeller,
   stripe_auth,
   stripeCustomer,
-  createCharge
-}
+  createCharge,
+};
