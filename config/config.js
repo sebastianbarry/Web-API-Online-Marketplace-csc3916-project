16fafb122483ea95e449ad00ba68140b092ee5ad
Polo <poloaguirre3421@gmail.com>
2022-03-28 20:38:17 -0600

    Added basic stuff

--- config/config.js
+++ config/config.js
@@ -1,15 +1,15 @@
 const config = {
-  env: process.env.NODE_ENV || 'development',
+  env: process.env.NODE_ENV || "development",
   port: process.env.PORT || 3000,
   jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
-  mongoUri: process.env.MONGODB_URI ||
-    process.env.MONGO_HOST ||
-    'mongodb://' + (process.env.IP || 'localhost') + ':' +
-    (process.env.MONGO_PORT || '27017') +
-    '/mernproject',
-  stripe_connect_test_client_id: 'YOUR_stripe_connect_test_client',
-  stripe_test_secret_key: 'YOUR_stripe_test_secret_key',
-  stripe_test_api_key: 'YOUR_stripe_test_api_key'
-}
+  mongoUri:
+    process.env.MONGODB_URI ||
+    "mongodb+srv://mongodb:mongodbuser@cluster0.73b3k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  stripe_connect_test_client_id: "ca_LPJOyZoCUFlmsoGOfumKmlquHGdmO1MO",
+  stripe_test_secret_key:
+    "sk_test_51KdPvFBqBuvo2VlvRKuz2RYgam0qTiaZapMa2n9PBg8xnHMU03JLBXvsD0d7mjgZ8UAvdUei5zf5H0Jr6fVo4Qhv00Sbd54Ppb",
+  stripe_test_api_key:
+    "pk_test_51KdPvFBqBuvo2VlvctLUF2yCFicwSsUCsH9HxPHmaXOJdns4hVGAdePl05UUSFYJf7dxTZQPERBoB5VoWyHYpb4G003Dysco1I",
+};
 
-export default config
+export default config;
